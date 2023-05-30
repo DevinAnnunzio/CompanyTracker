b67071abfdd3da4dd40cc6d1c7d1d61b849bd7c8
DevinAnnunzio <annunzio.da@gmail.comgit config --global user.name DevinAnnunzio git config --global user.name DevinAnnunzio git config --global user.email annunzio.da@gmail.com>
2023-05-30 15:53:44 +0900

    Fix: fixed issue with connecting to DB

--- company-tracker.sql
+++ company-tracker.sql
@@ -2,8 +2,14 @@ DROP TABLE IF EXISTS company;
 
 CREATE TABLE company (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  company_name VARCHAR(50) NOT NULL,
+  company_name VARCHAR(50) UNIQUE NOT NULL,
   ceo VARCHAR(50) DEFAULT NULL,
   stock_price DECIMAL(15,2) DEFAULT NULL,
   headquarters VARCHAR(80) DEFAULT NULL
 );
+
+
+
+INSERT INTO company (company_name) VALUES('Google');
+INSERT INTO company (company_name) VALUES('Rakuten');
+INSERT INTO company (company_name) VALUES('Facebook');
---
+++ src/com/annunzio/web/TestServlet.java
@@ -0,0 +1,67 @@
+package com.annunzio.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@Resource(name= "jdbc/company_tracker")
+	private DataSource dataSource;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Used to send data back to browser
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		
+		try {
+			//DataSource is same as connection pool
+			myConn = dataSource.getConnection();
+			
+			//Create SQL statement
+			String sqlStmt = "SELECT * FROM company";
+			myStmt = myConn.createStatement();
+			
+			//Execute SQL query
+			myRs = myStmt.executeQuery(sqlStmt);
+			
+			//Process sql result
+			while(myRs.next()) {
+				String email = myRs.getString("company_name");
+				out.println(email);
+			}
+			
+			
+		}  catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+
+
+}
